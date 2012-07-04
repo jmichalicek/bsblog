a835e32a42f9ad85a5c8ed342625de554be8ae8d
Justin Michalicek <jmichalicek@gmail.com>
2012-07-03 20:30:43 -0400

    Added more unit tests

--- bsblog/tests.py
+++ bsblog/tests.py
@@ -111,6 +111,12 @@ class ArchiveViewTests(TestCase):
 class IndexViewTests(TestCase):
     """Test the index view"""
 
+    fixtures = ['test_indexviewtests']
+
+    def setUp(self):
+        self.published_post = Post.objects.get(id=1)
+        self.unpublished_post = Post.objects.get(id=2)
+
     def test_get_response_code(self):
         """Make sure we get a 200 response code"""
         response = self.client.get(reverse('bsblog_main'))
@@ -119,6 +125,23 @@ class IndexViewTests(TestCase):
     def test_template(self):
         response = self.client.get(reverse('bsblog_main'))
         self.assertEqual(response.templates[0].name, 'bsblog/index.html')
+
+    def test_empty_page(self):
+        """Verify that a 404 is returned when a page with no results is requested"""
+        response = self.client.get(reverse('bsblog_main', args=[2]))
+        self.assertEqual(response.status_code, 404)
+
+    def test_context_post_list(self):
+        """Make sure our posts are in the post_list item in the context"""
+        response = self.client.get(reverse('bsblog_main'))
+        self.assertTrue('post_list' in response.context)
+        self.assertTrue(self.published_post in response.context['post_list'])
+
+    def test_unpublished_post(self):
+        """Make sure unpublished posts are NOT in the post_list"""
+        response = self.client.get(reverse('bsblog_main'))
+        self.assertFalse(self.unpublished_post in response.context['post_list'])
+
 
 class ItemViewTests(TestCase):
     """Test the item view"""
